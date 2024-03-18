d74eb20c2e3b56ae99022646318730a9abcd962d
muxuhang <17186765121@163.com>
2024-03-18 16:48:41 +0800

    article

--- src/components/Article/index.tsx
+++ src/components/Article/index.tsx
@@ -2,12 +2,20 @@ import React from 'react'
 import './sass/article.scss'
 interface ArticleProps {
   title: string
+  time?: string
+  author?: string
   children: string | React.ReactNode
 }
 export default function Article(props: ArticleProps) {
-  const { title, children } = props
+  const { title, author, time, children } = props
   return <article>
-    <h2>{title}</h2>
+    <hgroup>
+      <h1>{title}</h1>
+      <h2>
+        {author && <span>作者: {author}</span>}
+        {time && <time>时间: {time}</time>}
+      </h2>
+    </hgroup>
     {
       typeof children === 'string' ?
         <p>{children}</p> :
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -42,9 +42,9 @@ export default function HomePage() {
     <SdProvidor>
       <Uselect />
     </SdProvidor>
-    <Articli title='明朝那些事'>
+    <Articli title='明朝那些事' author='当年明月' time='2020-10-10'>
       《明朝那些事儿》是一套在网络上连载的关于中国明朝历史的作品，作者为当年明月。[注1][参1][参2]于2006年3月10日在天涯社区首次发表，讲述了明朝二百七十六年的历史，其内容始于明太祖朱元璋的诞生，终于崇祯帝朱由检在煤山自缢。《明朝那些事儿》在网络连载期间，每月点击率逾百万人次。[参3]后来，《明朝那些事儿》集结成书籍刊行，在台湾发行了繁体中文版本。之后被翻译为日文、韩文及英文等多国语言[参4]。
-      <code>code</code>
+      <br /><code>code</code><br />
       <strong>strong</strong><br />
       <em>em</em><br />
       <dfn>dfn</dfn><br />
@@ -52,7 +52,7 @@ export default function HomePage() {
       <kbd>kbd</kbd><br />
       <var>var</var><br />
       <time>2024-3-18</time><br />
-      <a>123</a>
+      <a href=''>123</a>
     </Articli>
   </div>
 }
